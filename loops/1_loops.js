233d90f414ceac1bbfb287303006031a3a7cc67f
Nordraf <rkamieniarz@gmail.com>
2016-11-11 15:54:54 +0100

    js loops

--- loops/1_loops.js
+++ loops/1_loops.js
@@ -1,8 +1,48 @@
-var countTo;
+var number = 12;
 
 /**
- * countTo
+ * While Loop
+ * @param countTo
  */
 function whileLoop (countTo) {
+    var i = 1;
 
+    while (i <= countTo) {
+        console.log(i + " ");
+        i += 1;
+    }
 }
+
+/**
+ * do while loop
+ * @param countTo
+ */
+function doWhileLoop (countTo) {
+    var i = 1;
+
+    do {
+        console.log(i + " ");
+        i += 1;
+    }
+    while (i <= countTo);
+}
+
+
+/**
+ * for loop
+ * @param countTo
+ */
+function forLoop (countTo) {
+    var i;
+
+    for (i=1; i <= countTo; i++) {
+        console.log(i);
+    }
+}
+
+
+forLoop(number);
+console.log('---------------------');
+whileLoop(number);
+console.log('---------------------');
+doWhileLoop(number);
