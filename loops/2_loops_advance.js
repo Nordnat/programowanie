f184f490301d5d598741d7a245a38a1734fafe59
Paweł Zadrożny <pawel.zny@gmail.com>
2016-11-12 14:01:08 +0100

    loops advanced

---
+++ loops/2_loops_advance.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/*
+ * Create two function with loops without using loop syntax.
+ * First function can have two parameters: limit, and offset.
+ * Second function must have only one parameter: limit.
+ *
+ * Each loop should console.log() from 0 to limit
+ */
