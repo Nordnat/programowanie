f1af14a40e660c24485f689943c1d2deb55e371d
Nordraf <rkamieniarz@gmail.com>
2016-11-14 21:44:40 +0100

    nowe zadanie nr 3

---
+++ loops/3_loops_array.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/*
+ * Create all kind of array loop. Each loop in it's own function with one parameter.
+ *
+ * Each loop should console.log() every single item in given array.
+ */
---
+++ loops/3_loops_array.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+
+<?php
+
+/*
+ * Create all kind of array loop. Each loop in it's own function with one parameter.
+ *
+ * Each loop should print() every single item in given array.
+ */
