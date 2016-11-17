5d73dc2266c1a69e07aac05baeeed2ff05bb28e9
Nordraf <rkamieniarz@gmail.com>
2016-11-17 19:58:17 +0100

    zadanie 2 (poprawki) oraz zadanie 3

--- loops/3_loops_array.js
+++ loops/3_loops_array.js
@@ -5,3 +5,89 @@
  *
  * Each loop should console.log() every single item in given array.
  */
+
+
+var mainArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ *
+ * @param (array) sets array to iterate
+ */
+function forLoop (array) {
+    for (let arr of array) {
+        console.log(arr);
+    }
+}
+
+/**
+ *
+ * @param (array) sets array to iterate
+ */
+function forInLoop (array) {
+    for (let arr in array) {
+        if (array.hasOwnProperty(arr)) {
+            console.log(arr);
+        }
+    }
+}
+
+/**
+ *
+ * (array) sets array to iterate
+ */
+function standardForLoop (array) {
+    for (let index = 0; index < array.length; index++) {
+        console.log(array[index]);
+    }
+}
+
+/**
+ *
+ * @param (array) sets array to iterate
+ */
+function forEachLoop (array) {
+    array.forEach(function(arrayElement) {
+        console.log(arrayElement);
+    });
+}
+
+/**
+ *
+ * @param array (array) sets array to iterate
+ */
+function whileLoop (array) {
+    let arrayLength = array.length,
+        i = 0;
+
+    while (i < arrayLength) {
+        console.log(array[i]);
+        i += 1;
+    }
+}
+
+/**
+ *
+ * @param array
+ */
+function doWhileLoop (array) {
+    let arrayLength = array.length,
+        i = 0;
+
+    do {
+        console.log(array[i]);
+        i += 1;
+    } while (i < arrayLength);
+}
+
+
+forLoop(mainArray);
+console.log('---------------------');
+forInLoop(mainArray);
+console.log('---------------------');
+standardForLoop(mainArray);
+console.log('---------------------');
+forEachLoop(mainArray);
+console.log('---------------------');
+whileLoop(mainArray);
+console.log('---------------------');
+doWhileLoop(mainArray);
--- loops/3_loops_array.php
+++ loops/3_loops_array.php
@@ -7,3 +7,53 @@
  *
  * Each loop should print() every single item in given array.
  */
+
+$mainArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * @param (array) sets array to iterate
+ */
+function forEachLoop($array) {
+    foreach ($array as $arr) {
+        print("$arr\n");
+    }
+}
+
+/**
+ * @param (array) sets array to iterate
+ */
+function forLoop($array) {
+    $arrayLength = count($array);
+    for ($i = 0; $i < $arrayLength; $i++) {
+        print("$array[$i]\n");
+    }
+}
+
+/**
+ * @param (array) sets array to iterate
+ */
+function whileLoop($array) {
+    $i = 0;
+    $arrayLength = count($array);
+    while ($i < $arrayLength) {
+        print("$array[$i]\n");
+        $i++;
+    }
+}
+
+/**
+ * @param (array) sets array to iterate
+ */
+function doWhileLoop($array) {
+    $i = 0;
+    $arrayLength = count($array);
+    do {
+        print($i += 1);
+    } while ($i < $arrayLength);
+}
+
+
+forEachLoop($mainArray);
+forLoop($mainArray);
+whileLoop($mainArray);
+doWhileLoop($mainArray);
